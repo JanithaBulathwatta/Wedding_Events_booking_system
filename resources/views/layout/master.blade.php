34fc4e28fce7f9e19d1cf3b12a08ebfdc09f1526
janitha Bulathwatta <janithabulathwatta04@gmail.com>
2026-06-04 22:05:58 +0530

    create folder structure

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+    @include('libraries.styles')
+    @yield('customCSS')
+<body>
+    @yield('content')
+
+
+    @include('libraries.script')
+    @yield('customJS')
+        <script>
+                $(document).ready(function() {
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    });
+                    // $("#overlay-spinner").fadeIn(100);
+                    // init();
+                    // events();
+                    // validations();
+                });
+        </script>
+</body>
+
+</html>
